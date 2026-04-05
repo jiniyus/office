0b0d92277fccb29aa2b2d0c74396d21637d26f5a
pranaypugalia06 <pranaypugalia06@gmail.com>
2026-04-05 17:46:30 +0530

    updated capitalize to preserve original casing

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -7,8 +7,5 @@ export function cn(...inputs: ClassValue[]) {
 
 export function capitalize(str: string = ''): string {
   if (!str) return str
-  return str
-    .split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ')
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
