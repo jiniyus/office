6d0cde753306ac8c689110739ed210a91d476bf3
pranaypugalia06 <pranaypugalia06@gmail.com>
2026-04-15 18:44:23 +0530

    v5.1

--- client/src/components/ui/searchable-select.tsx
+++ client/src/components/ui/searchable-select.tsx
@@ -93,17 +93,17 @@ export function SearchableSelect({
           </div>
         </div>
       </PopoverTrigger>
-      <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0 max-h-[80vh] md:max-h-[300px] overflow-hidden flex flex-col z-50" align="start">
+      <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0 max-h-[200px] overflow-hidden flex flex-col z-50" align="start">
         <Command className="overflow-hidden flex flex-col">
           <CommandEmpty>No items found.</CommandEmpty>
-          <CommandGroup className="max-h-[calc(80vh-0px)] md:max-h-[280px] overflow-y-auto overscroll-contain flex-1">
+          <CommandGroup className="max-h-[180px] overflow-y-auto overscroll-contain flex-1">
             {filteredItems.length > 0 ? (
               filteredItems.map((item) => (
                 <CommandItem
                   key={item.id}
                   value={item.id}
                   onSelect={() => handleSelect(item.id)}
-                  className="text-xs cursor-pointer"
+                  className="text-xs cursor-pointer py-2"
                 >
                   <Check
                     className={cn(
